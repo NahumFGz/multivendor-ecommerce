53f00241ea29f993cdeb14b29843dc113e95f6ae
NahumFG <ngutierrez2507@gmail.com>
2024-06-02 15:21:47 -0500

    backend: config swagger

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -14,9 +14,35 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+from rest_framework import permissions
+
+get_schema_view = get_schema_view(
+    openapi.Info(
+        title="E-commerce Backend APIs",
+        default_version="v1",
+        description="This is documentation for the E-commerce Backend APIs",
+        terms_of_service="https://www.google.com/policies/terms/",
+        contact=openapi.Contact(email="test1@mail.com", name="test1"),
+        license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    # URLs de la documentación
+    path("", get_schema_view.with_ui("swagger", cache_timeout=0), name="schema-swagger-ui"),
+    # URLs de las aplicaciones
+    path("admin/", admin.site.urls),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
