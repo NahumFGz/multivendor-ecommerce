8c55f09ac4f270fb999799cbd4519d37ece6b50f
NahumFG <ngutierrez2507@gmail.com>
2024-06-04 14:04:12 -0500

    backend: model vendor

--- backend/apps/product_vendor/admin.py
+++ backend/apps/product_vendor/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import ProductVendor
+
+
+@admin.register(ProductVendor)
+class ProductVendorAdmin(admin.ModelAdmin):
+    readonly_fields = [
+        "image_small_size",
+        "image_medium_size",
+        "image_large_size",
+        "created_at",
+        "updated_at",
+    ]
--- backend/apps/product_vendor/models.py
+++ backend/apps/product_vendor/models.py
@@ -1,3 +1,40 @@
+from datetime import timedelta
+
+from apps.product.models import Product
+from django.contrib.auth import get_user_model
 from django.db import models
+from django.db.models import signals
+from django.dispatch import receiver
+from django.utils import timezone
+from django.utils.text import slugify
+
+User = get_user_model()
+
 
 # Create your models here.
+class ProductVendor(Product):
+    vendor = models.ForeignKey(User, related_name="products", on_delete=models.CASCADE)
+    days_in_publication = models.PositiveIntegerField(default=30)
+    last_date_in_publication = models.DateTimeField(blank=True, null=True)
+
+    class Meta:
+        verbose_name = "Product Vendor"
+        verbose_name_plural = "Products Vendor"
+        ordering = ["-updated_at"]
+
+    def __str__(self):
+        return self.vendor.email + " - " + self.title
+
+
+@receiver(signals.pre_save, sender=Product)
+def pre_save_product(sender, instance, **kwargs):
+    instance.slug = slugify(instance.title)
+
+
+@receiver(signals.pre_save, sender=ProductVendor)
+def pre_save_product_vendor(sender, instance, **kwargs):
+    if instance.last_date_in_publication is None:
+        instance.last_date_in_publication = timezone.now()
+    instance.last_date_in_publication = instance.last_date_in_publication + timedelta(
+        days=instance.days_in_publication
+    )
