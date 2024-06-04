b509209c21c4841a18a40d66d5bc3fcce29839ce
NahumFG <ngutierrez2507@gmail.com>
2024-06-04 11:46:37 -0500

    backend: add images to serializer

--- backend/apps/product/api_rest/serializers.py
+++ backend/apps/product/api_rest/serializers.py
@@ -22,6 +22,29 @@ class CategorySerializer(serializers.ModelSerializer):
 
 
 class ProductSerializer(serializers.ModelSerializer):
+    images = serializers.SerializerMethodField()
+
     class Meta:
         model = Product
-        fields = "__all__"
+        fields = (
+            "id",
+            "category",
+            "kind_of_product",
+            "sub_kind_of_product",
+            "title",
+            "slug",
+            "short_description",
+            "description",
+            "views",
+            "rating",
+            "images",
+            "updated_at",
+        )
+
+    def get_images(self, obj):
+        return {
+            "principal": obj.image_principal.url if obj.image_principal else None,
+            "small": obj.image_small_size if obj.image_principal else None,
+            "medium": obj.image_medium_size if obj.image_principal else None,
+            "large": obj.image_large_size if obj.image_principal else None,
+        }
