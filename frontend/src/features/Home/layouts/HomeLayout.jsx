64ec0d36ef7107dca6e302dd5930e6c3efa4ce83
NahumFG <ngutierrez2507@gmail.com>
2024-06-05 21:57:26 -0500

    frontend: add filters componet

--- frontend/src/features/Home/layouts/HomeLayout.jsx
+++ frontend/src/features/Home/layouts/HomeLayout.jsx
@@ -1,9 +1,11 @@
+import { Filters } from '../components/Filters/Filters'
 import { Header } from '../components/Header/Header'
 
 export function HomeLayout ({ children }) {
   return (
     <div className='flex flex-col h-screen'>
       <Header />
+      <Filters />
       <div className='flex-1 overflow-y-auto'>
         {children}
       </div>
