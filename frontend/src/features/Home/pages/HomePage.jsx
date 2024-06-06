69257f7a190d022f8a74b7e43b240d7cba47b29d
NahumFG <ngutierrez2507@gmail.com>
2024-06-06 11:39:39 -0500

    frontend: dart theme store

--- frontend/src/features/Home/pages/HomePage.jsx
+++ frontend/src/features/Home/pages/HomePage.jsx
@@ -1,7 +1,16 @@
+import { useSwapTheme } from '../../../store/ThemeStore'
+
 export function HomePage () {
+  const { handleSwapTheme } = useSwapTheme()
+
   return (
     <div>
       <h1>Home Page</h1>
+      <button
+        className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded'
+        onClick={handleSwapTheme}
+      >Cambiar tema
+      </button>
     </div>
   )
 }
