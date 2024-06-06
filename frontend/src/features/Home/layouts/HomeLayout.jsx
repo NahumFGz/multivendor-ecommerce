249ad495eac647fd3b9a8fbc01db358f3f28372e
NahumFG <ngutierrez2507@gmail.com>
2024-06-05 22:08:33 -0500

    frontend: add dark mode

--- frontend/src/features/Home/layouts/HomeLayout.jsx
+++ frontend/src/features/Home/layouts/HomeLayout.jsx
@@ -3,7 +3,7 @@ import { Header } from '../components/Header/Header'
 
 export function HomeLayout ({ children }) {
   return (
-    <div className='flex flex-col h-screen'>
+    <div className='flex flex-col'>
       <Header />
       <Filters />
       <div className='flex-1 overflow-y-auto'>
