3ef3c5dfc7545c1b8033cdb5a8391b7ad0cf6137
NahumFG <ngutierrez2507@gmail.com>
2024-06-06 22:05:32 -0500

    frontend: add initial sidebar

--- frontend/src/features/Account/layouts/AccountLayout.jsx
+++ frontend/src/features/Account/layouts/AccountLayout.jsx
@@ -1,7 +1,10 @@
+import { LateralBar } from '../components/LateralBar'
+
 export function AccountLayout ({ children }) {
   return (
     <>
       <h1>Account Layout</h1>
+      <LateralBar />
       <div>
         {children}
       </div>
