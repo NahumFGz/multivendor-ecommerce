411c99b8071cde26f714b05015984be2fd1377e8
NahumFG <ngutierrez2507@gmail.com>
2024-06-09 04:01:53 -0500

    frontend: init AuthLayout

--- frontend/src/features/Auth/layouts/AuthLayout.jsx
+++ frontend/src/features/Auth/layouts/AuthLayout.jsx
@@ -1,10 +1,18 @@
+import { Footer } from '../../../components/Footer'
+import { Header } from '../../../components/Header'
+
 export function AuthLayout ({ children }) {
   return (
-    <>
-      <h1>Auth Layout</h1>
+    <div className='flex flex-col'>
       <div>
+        <Header />
+      </div>
+      <div className='mt-10 h-[700px]'>
         {children}
       </div>
-    </>
+      <div>
+        <Footer />
+      </div>
+    </div>
   )
 }
