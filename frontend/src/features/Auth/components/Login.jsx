c3f461d2eea4ab45ad4d9486d784cb60d68af4d9
NahumFG <ngutierrez2507@gmail.com>
2024-06-09 19:26:52 -0500

    frontend: login validation

--- frontend/src/features/Auth/components/Login.jsx
+++ frontend/src/features/Auth/components/Login.jsx
@@ -1,29 +1,54 @@
-import React from 'react'
 import { Button, Input, Checkbox, Link, Divider } from '@nextui-org/react'
 import { Icon } from '@iconify/react'
-
 import { AcmeIcon } from '../../../assets/Social'
+import { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+import { authUrls } from '../../../routes/urls/authUrls'
+import { useFormik } from 'formik'
+import * as Yup from 'yup'
+
+const validationSchema = Yup.object().shape({
+  email: Yup.string().email('Invalid email address').required('Email is required'),
+  password: Yup.string().min(6, 'Password must be at least 6 characters').required('Password is required')
+})
 
 export function Login () {
-  const [isVisible, setIsVisible] = React.useState(false)
-
+  const navigate = useNavigate()
+  const [isVisible, setIsVisible] = useState(false)
   const toggleVisibility = () => setIsVisible(!isVisible)
 
+  const handleNavigate = (url) => {
+    navigate(url)
+  }
+
+  const formik = useFormik({
+    initialValues: { email: '', password: '', remember: false },
+    validationSchema,
+    onSubmit: (values) => {
+      // Handle form submission
+      console.log(values)
+    }
+  })
+
   return (
-    <div className='flex h-full  w-full flex-col items-center justify-center'>
+    <div className='flex h-full w-full flex-col items-center justify-center'>
       <div className='flex flex-col items-center pb-6'>
         <AcmeIcon size={60} />
         <p className='text-xl font-medium'>Welcome Back</p>
         <p className='text-small text-default-500'>Log in to your account to continue</p>
       </div>
       <div className='mt-2 flex w-full max-w-sm flex-col gap-4 rounded-large bg-content1 px-8 py-6 shadow-small'>
-        <form className='flex flex-col gap-3' onSubmit={(e) => e.preventDefault()}>
+        <form className='flex flex-col gap-3' onSubmit={formik.handleSubmit}>
           <Input
             label='Email Address'
             name='email'
             placeholder='Enter your email'
             type='email'
-            variant='bordered'
+            variant={formik.touched.email && formik.errors.email ? 'flat' : 'bordered'}
+            color={formik.touched.email && formik.errors.email ? 'danger' : 'default'}
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+            value={formik.values.email}
           />
 
           <Input
@@ -48,14 +73,29 @@ export function Login () {
             name='password'
             placeholder='Enter your password'
             type={isVisible ? 'text' : 'password'}
-            variant='bordered'
+            variant={formik.touched.password && formik.errors.password ? 'flat' : 'bordered'}
+            color={formik.touched.password && formik.errors.password ? 'danger' : 'default'}
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+            value={formik.values.password}
+            className={formik.touched.password && formik.errors.password ? 'border-red-500' : ''}
           />
 
           <div className='flex items-center justify-between px-1 py-2'>
-            <Checkbox name='remember' size='sm'>
+            <Checkbox
+              name='remember'
+              size='sm'
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
+              checked={formik.values.remember}
+            >
               Remember me
             </Checkbox>
-            <Link className='text-default-500' href='#' size='sm'>
+            <Link
+              className='text-default-500 cursor-pointer'
+              size='sm'
+              onPress={() => handleNavigate(authUrls.forgotPassword)}
+            >
               Forgot password?
             </Link>
           </div>
@@ -79,7 +119,11 @@ export function Login () {
         </div>
         <p className='text-center text-small'>
           Need to create an account?&nbsp;
-          <Link href='#' size='sm'>
+          <Link
+            size='sm'
+            className='cursor-pointer'
+            onPress={() => handleNavigate(authUrls.register)}
+          >
             Sign Up
           </Link>
         </p>
