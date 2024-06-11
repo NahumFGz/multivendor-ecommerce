bb05541b91769fb2080d39684fe74b1af8b78cff
NahumFG <ngutierrez2507@gmail.com>
2024-06-11 12:56:19 -0500

    frontend: center home layout

--- frontend/src/features/Home/layouts/HomeLayout.jsx
+++ frontend/src/features/Home/layouts/HomeLayout.jsx
@@ -7,7 +7,7 @@ export function HomeLayout ({ children }) {
       <div>
         <Header />
       </div>
-      <div className='mt-3'>
+      <div className='flex items-center justify-center mt-3'>
         {children}
       </div>
       <div>
