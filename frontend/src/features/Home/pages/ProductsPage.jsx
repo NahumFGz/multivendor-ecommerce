ef8e8ad634b6dae8701eebc91cbf58a6181c155e
NahumFG <ngutierrez2507@gmail.com>
2024-06-14 21:24:48 -0500

    mx mt products page

--- frontend/src/features/Home/pages/ProductsPage.jsx
+++ frontend/src/features/Home/pages/ProductsPage.jsx
@@ -3,7 +3,7 @@ import { Products } from '../components/Products/Products'
 
 export function ProductsPage () {
   return (
-    <div>
+    <div className='mx-12 mt-2'>
       <Filters />
       <div className='block rounded-medium border-medium border-dashed border-divider m-2'>
         <Products />
