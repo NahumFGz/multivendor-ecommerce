4db643d5f16a60a633275870ad4d4673c6ebd2c4
NahumFG <ngutierrez2507@gmail.com>
2024-06-14 21:36:54 -0500

    add pagination

--- frontend/src/features/Home/pages/ProductsPage.jsx
+++ frontend/src/features/Home/pages/ProductsPage.jsx
@@ -5,7 +5,7 @@ export function ProductsPage () {
   return (
     <div className='mx-12 mt-2'>
       <Filters />
-      <div className='block rounded-medium border-medium border-dashed border-divider m-2'>
+      <div>
         <Products />
       </div>
     </div>
