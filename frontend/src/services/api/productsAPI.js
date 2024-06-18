16d37c09281883f41846bc43bd8a199d1a631928
NahumFG <ngutierrez2507@gmail.com>
2024-06-18 14:14:46 -0500

    frontend: init productsApi

---
+++ frontend/src/services/api/productsAPI.js
@@ -0,0 +1,33 @@
+import { createAxiosInstance } from './axiosInstance'
+
+export async function getProductsApi () {
+  try {
+    const axiosInstance = createAxiosInstance()
+    const response = await axiosInstance.get('/api/products/')
+    const { data, status } = response
+
+    if (status === 200) {
+      return data
+    } else {
+      throw new Error('Get products failed')
+    }
+  } catch (error) {
+    throw new Error('Get products failed')
+  }
+}
+
+export async function getProductDetailAPpi (slugProduct) {
+  try {
+    const axiosInstance = createAxiosInstance()
+    const response = await axiosInstance.patch(`/api/products/${slugProduct}/`)
+    const { data, status } = response
+
+    if (status === 200) {
+      return data
+    } else {
+      throw new Error('Get product detail failed')
+    }
+  } catch (error) {
+    throw new Error('Get product detail failed')
+  }
+}
