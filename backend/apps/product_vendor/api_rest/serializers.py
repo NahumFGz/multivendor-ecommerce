5a39e589163fe34c7cbef72b13fb9b572c011af7
NahumFG <ngutierrez2507@gmail.com>
2024-06-18 12:17:04 -0500

    backend: refact product vendor

--- backend/apps/product_vendor/api_rest/serializers.py
+++ backend/apps/product_vendor/api_rest/serializers.py
@@ -47,3 +47,40 @@ class ProductVendorSerializer(serializers.ModelSerializer):
             "medium": obj.image_medium_size if obj.image_principal else None,
             "large": obj.image_large_size if obj.image_principal else None,
         }
+
+
+class ProductVendorDetailSerializer(serializers.ModelSerializer):
+    images = serializers.SerializerMethodField()
+    category = TinyCategorySerializer()
+    kind_of_product = TinyKindProductSerializer()
+    sub_kind_of_product = TinySubKindProductSerializer()
+    vendor = TinyUserSerializer()
+
+    class Meta:
+        model = ProductVendor
+        fields = (
+            "id",
+            "vendor",
+            "category",
+            "kind_of_product",
+            "sub_kind_of_product",
+            "title",
+            "short_description",
+            "description",
+            "slug",
+            "price",
+            "views",
+            "rating",
+            "days_in_publication",
+            "updated_at",
+            "last_date_in_publication",
+            "images",
+        )
+
+    def get_images(self, obj):
+        return {
+            "principal": obj.image_principal.url if obj.image_principal else None,
+            "small": obj.image_small_size if obj.image_principal else None,
+            "medium": obj.image_medium_size if obj.image_principal else None,
+            "large": obj.image_large_size if obj.image_principal else None,
+        }
--- backend/apps/product_vendor/api_rest/views.py
+++ backend/apps/product_vendor/api_rest/views.py
@@ -1,22 +1,70 @@
-from rest_framework import viewsets
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import generics, mixins, viewsets
 from rest_framework.exceptions import NotFound
+from rest_framework.filters import OrderingFilter, SearchFilter
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
 
 from ..models import ProductVendor
-from .serializers import ProductVendorSerializer
+from .serializers import ProductVendorDetailSerializer, ProductVendorSerializer
 
 
-class ProductVendorViewSet(viewsets.ModelViewSet):
+class ProductVendorListView(generics.ListAPIView):
     queryset = ProductVendor.objects.all()
     serializer_class = ProductVendorSerializer
     permission_classes = [IsAuthenticatedOrReadOnly]
-    http_method_names = ["get", "post", "patch"]
-    lookup_field = "slug"
+    filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
 
-    def get_object(self):
-        queryset = self.filter_queryset(self.get_queryset())
-        slug = self.kwargs.get(self.lookup_field)
-        try:
-            return queryset.get(slug=slug)
-        except ProductVendor.DoesNotExist:
-            raise NotFound(detail="ProductVendor not found.")
+    # Configurar los campos de búsqueda
+    search_fields = ["title", "description"]
+
+    # Configurar los campos de filtro
+    filterset_fields = ["category", "price", "rating"]
+
+    # Configurar los campos de ordenamiento y ordenamiento predeterminado
+    ordering_fields = ["price", "rating", "created_at"]
+    ordering = ["created_at"]
+
+
+class ProductVendorDetailView(generics.RetrieveAPIView):
+    """
+    RetrieveAPIView es una clase de vista genérica que
+    proporciona una interfaz de solo lectura para un recurso.
+    """
+
+    lookup_field = "slug"
+    queryset = ProductVendor.objects.all()
+    serializer_class = ProductVendorDetailSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]
+
+
+class ProductVendorCRUDView(
+    mixins.CreateModelMixin,
+    mixins.UpdateModelMixin,
+    mixins.DestroyModelMixin,
+    generics.GenericAPIView,
+):
+    queryset = ProductVendor.objects.all()
+    serializer_class = ProductVendorSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]
+
+    def post(self, request, *args, **kwargs):
+        return self.create(request, *args, **kwargs)
+
+    def patch(self, request, *args, **kwargs):
+        return self.partial_update(request, *args, **kwargs)
+
+
+# class ProductVendorViewSet(viewsets.ModelViewSet):
+#     queryset = ProductVendor.objects.all()
+#     serializer_class = ProductVendorSerializer
+#     permission_classes = [IsAuthenticatedOrReadOnly]
+#     http_method_names = ["get", "post", "patch"]
+#     lookup_field = "slug"
+
+#     def get_object(self):
+#         queryset = self.filter_queryset(self.get_queryset())
+#         slug = self.kwargs.get(self.lookup_field)
+#         try:
+#             return queryset.get(slug=slug)
+#         except ProductVendor.DoesNotExist:
+#             raise NotFound(detail="ProductVendor not found.")
