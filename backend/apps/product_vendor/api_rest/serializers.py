f535bf39f6436268c864ed857e3f08db2c688338
NahumFG <ngutierrez2507@gmail.com>
2024-06-17 22:04:09 -0500

    backend: get products_vendor

--- backend/apps/product_vendor/api_rest/serializers.py
+++ backend/apps/product_vendor/api_rest/serializers.py
@@ -4,6 +4,31 @@ from ..models import ProductVendor
 
 
 class ProductVendorSerializer(serializers.ModelSerializer):
+    images = serializers.SerializerMethodField()
+
     class Meta:
         model = ProductVendor
-        fields = "__all__"
+        fields = (
+            "id",
+            "vendor",
+            "category",
+            "kind_of_product",
+            "sub_kind_of_product",
+            "title",
+            "slug",
+            "price",
+            "views",
+            "rating",
+            "days_in_publication",
+            "updated_at",
+            "last_date_in_publication",
+            "images",
+        )
+
+    def get_images(self, obj):
+        return {
+            "principal": obj.image_principal.url if obj.image_principal else None,
+            "small": obj.image_small_size if obj.image_principal else None,
+            "medium": obj.image_medium_size if obj.image_principal else None,
+            "large": obj.image_large_size if obj.image_principal else None,
+        }
