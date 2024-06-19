6d7c556f024709401a5bbac1647beba0757304d4
NahumFG <ngutierrez2507@gmail.com>
2024-06-19 18:18:25 -0500

    backend: add price filter

--- backend/apps/product/api_rest/views.py
+++ backend/apps/product/api_rest/views.py
@@ -1,3 +1,4 @@
+import django_filters
 from common.pagination import CustomPageNumberPagination
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import generics, mixins, viewsets
@@ -33,6 +34,15 @@ class CategoryViewSet(generics.ListAPIView):
     permission_classes = [IsAuthenticatedOrReadOnly]
 
 
+class ProductFilter(django_filters.FilterSet):
+    price_min = django_filters.NumberFilter(field_name="price", lookup_expr="gte")
+    price_max = django_filters.NumberFilter(field_name="price", lookup_expr="lte")
+
+    class Meta:
+        model = Product
+        fields = ["category", "price_min", "price_max", "rating"]
+
+
 class ProductListView(generics.ListAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
@@ -44,7 +54,7 @@ class ProductListView(generics.ListAPIView):
     search_fields = ["title", "description"]
 
     # Configurar los campos de filtro
-    filterset_fields = ["category", "price", "rating"]
+    filterset_class = ProductFilter
 
     # Configurar los campos de ordenamiento y ordenamiento predeterminado
     ordering_fields = ["price", "rating", "created_at"]
