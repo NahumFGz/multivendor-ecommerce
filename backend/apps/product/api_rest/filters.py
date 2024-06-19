44d21bf99d7236073a0ca32ff867d4e2b815195c
NahumFG <ngutierrez2507@gmail.com>
2024-06-19 18:29:29 -0500

    backend: add kind an sub-kind to multiple filters

--- backend/apps/product/api_rest/filters.py
+++ backend/apps/product/api_rest/filters.py
@@ -11,7 +11,16 @@ class ProductFilter(django_filters.FilterSet):
     price_min = django_filters.NumberFilter(field_name="price", lookup_expr="gte")
     price_max = django_filters.NumberFilter(field_name="price", lookup_expr="lte")
     categories = NumberInFilter(field_name="category__id", lookup_expr="in")
+    kind_of_product = NumberInFilter(field_name="kind_of_product__id", lookup_expr="in")
+    sub_kind_of_product = NumberInFilter(field_name="sub_kind_of_product__id", lookup_expr="in")
 
     class Meta:
         model = Product
-        fields = ["categories", "price_min", "price_max", "rating"]
+        fields = [
+            "categories",
+            "kind_of_product",
+            "sub_kind_of_product",
+            "price_min",
+            "price_max",
+            "rating",
+        ]
