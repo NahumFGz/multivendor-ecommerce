f55da3a668df0ffb5b568f5408537e3549910204
NahumFG <ngutierrez2507@gmail.com>
2024-06-19 18:57:09 -0500

    backend: add TinySerializer

--- backend/apps/product/api_rest/views.py
+++ backend/apps/product/api_rest/views.py
@@ -13,6 +13,9 @@ from .serializers import (
     ProductDetailSerializer,
     ProductSerializer,
     SubKindProductSerializer,
+    TinyCategorySerializer,
+    TinyKindProductSerializer,
+    TinySubKindProductSerializer,
 )
 
 
@@ -39,17 +42,48 @@ class ProductListView(generics.ListAPIView):
     serializer_class = ProductSerializer
     permission_classes = [IsAuthenticatedOrReadOnly]
     filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
-    pagination_class = CustomPageNumberPagination
+    pagination_class = CustomPageNumberPagination  # Usar la clase de paginación personalizada
 
     # Configurar los campos de búsqueda
     search_fields = ["title", "description"]
 
     # Configurar los campos de filtro
-    filterset_class = ProductFilter
+    filterset_class = ProductFilter  # Usar el filtro personalizado
 
     # Configurar los campos de ordenamiento y ordenamiento predeterminado
     ordering_fields = ["price", "rating", "updated_at"]
     ordering = ["updated_at"]
+
+    def list(self, request, *args, **kwargs):
+        queryset = self.filter_queryset(self.get_queryset())
+
+        # Paginación
+        page = self.paginate_queryset(queryset)
+        if page is not None:
+            serializer = self.get_serializer(page, many=True)
+            product_data = self.get_paginated_response(serializer.data)
+        else:
+            serializer = self.get_serializer(queryset, many=True)
+            product_data = Response(serializer.data)
+
+        # Obtener todas las categorías, kinds y sub_kinds filtradas
+        categories = Category.objects.filter(product_category__in=queryset).distinct()
+        kinds = KindProduct.objects.filter(product_kind__in=queryset).distinct()
+        sub_kinds = SubKindProduct.objects.filter(product_subkind__in=queryset).distinct()
+
+        category_serializer = TinyCategorySerializer(categories, many=True)
+        kind_serializer = TinyKindProductSerializer(kinds, many=True)
+        sub_kind_serializer = TinySubKindProductSerializer(sub_kinds, many=True)
+
+        # Combinar los datos de productos con los datos adicionales
+        response_data = {
+            "products": product_data.data,
+            "categories": category_serializer.data,
+            "kinds": kind_serializer.data,
+            "sub_kinds": sub_kind_serializer.data,
+        }
+
+        return Response(response_data)
 
 
 class ProductDetailView(generics.RetrieveAPIView):
