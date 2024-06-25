0d98b345a44962a5a0942ff4801f18d3d5c6963e
NahumFG <ngutierrez2507@gmail.com>
2024-06-24 23:30:52 -0500

    frontend: init Layout filters

--- frontend/src/features/Home/layouts/HomeLayout.jsx
+++ frontend/src/features/Home/layouts/HomeLayout.jsx
@@ -1,12 +1,21 @@
 import { Footer } from '../components/Footer/Footer'
 import { Header } from '../components/Header/Header'
+import { useLocation } from 'react-router-dom'
+import { homeUrls } from '../../../routes/urls/homeUrls'
 
 export function HomeLayout ({ children }) {
+  const location = useLocation()
+
   return (
     <div className='flex flex-col'>
       <div>
         <Header />
       </div>
+      {location.pathname !== homeUrls.home && (
+        <div>
+          Filters
+        </div>
+      )}
       <div>
         {children}
       </div>
