8746819fad1253e7ad6be001cfc989c52e64dac5
NahumFG <ngutierrez2507@gmail.com>
2024-07-07 15:54:55 -0500

    frontend: init product info

---
+++ frontend/src/features/Home/components/ProductInfo/color-radio-item.jsx
@@ -0,0 +1,36 @@
+import { Tooltip, VisuallyHidden, useRadio } from '@nextui-org/react'
+
+import { cn } from '../../../../services/utilities/cn'
+import { forwardRef } from 'react'
+
+const ColorRadioItem = forwardRef(({ color, tooltip, ...props }, ref) => {
+  const { Component, isSelected, isFocusVisible, getBaseProps, getInputProps } = useRadio(props)
+
+  return (
+    <Tooltip content={tooltip} delay={1000} isDisabled={!tooltip} offset={0} placement='top'>
+      <Component {...getBaseProps()} ref={ref}>
+        <VisuallyHidden>
+          <input {...getInputProps()} />
+        </VisuallyHidden>
+        <span
+          className={cn(
+            'pointer-events-none h-8 w-8 rounded-full border border-black border-opacity-10 transition-transform group-data-[pressed=true]:scale-90',
+            {
+              'ring-2 ring-offset-2 ring-offset-content1': isSelected
+            }
+          )}
+          style={{
+            backgroundColor: color,
+            // @ts-ignore
+            '--tw-ring-color':
+              isSelected || isFocusVisible ? 'hsl(var(--nextui-primary))' : 'transparent'
+          }}
+        />
+      </Component>
+    </Tooltip>
+  )
+})
+
+ColorRadioItem.displayName = 'ColorRadioItem'
+
+export default ColorRadioItem
