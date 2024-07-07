be67ff58d98f2908c807cd348b00e88f49bb9360
NahumFG <ngutierrez2507@gmail.com>
2024-07-06 22:20:21 -0500

    frontend: clear params

--- frontend/src/features/Home/layouts/HomeLayout.jsx
+++ frontend/src/features/Home/layouts/HomeLayout.jsx
@@ -1,6 +1,6 @@
 import { Footer } from '../components/Footer/Footer'
 import { Header } from '../components/Header/Header'
-import { useLocation, useNavigate } from 'react-router-dom'
+import { useLocation } from 'react-router-dom'
 import { homeUrls } from '../../../routes/urls/homeUrls'
 import { Filters } from '../components/Filters/Filters'
 import { useState, useEffect } from 'react'
@@ -17,38 +17,23 @@ export function HomeLayout ({ children }) {
   })
   const [totalProducts, setTotalProducts] = useState(0)
 
-  const navigate = useNavigate()
-
-  const updateUrlParams = (params) => {
-    const searchParams = new URLSearchParams(location.search)
-    if (params.ordering) {
-      searchParams.set('ordering', params.ordering)
-    } else {
-      searchParams.delete('ordering')
+  const fetchProducts = async (params) => {
+    try {
+      const response = await getProducts(1, 10, params.ordering, params.selectedCategories, params.selectedSubCategories)
+      setTotalProducts(response.totalProducts)
+    } catch (error) {
+      console.error(error)
     }
-    if (params.selectedCategories.length > 0) {
-      searchParams.set('categories', params.selectedCategories.join(','))
-    } else {
-      searchParams.delete('categories')
-    }
-    if (params.selectedSubCategories.length > 0) {
-      searchParams.set('sub_categories', params.selectedSubCategories.join(','))
-    } else {
-      searchParams.delete('sub_categories')
-    }
-    navigate({
-      pathname: location.pathname,
-      search: searchParams.toString()
-    })
   }
 
-  const getQueryParams = () => {
-    const searchParams = new URLSearchParams(location.search)
-    const ordering = searchParams.get('ordering') || ''
-    const selectedCategories = searchParams.get('categories') ? searchParams.get('categories').split(',').map(Number) : []
-    const selectedSubCategories = searchParams.get('sub_categories') ? searchParams.get('sub_categories').split(',').map(Number) : []
-    return { ordering, selectedCategories, selectedSubCategories }
-  }
+  useEffect(() => {
+    fetchProducts(filterParams)
+  }, [filterParams])
+
+  useEffect(() => {
+    const filterTitle = getFilterTitle(location.pathname)
+    setFilterParams((prevParams) => ({ ...prevParams, filterTitle }))
+  }, [location.pathname])
 
   const getFilterTitle = (pathname) => {
     if (pathname === homeUrls.products) {
@@ -62,30 +47,8 @@ export function HomeLayout ({ children }) {
     }
   }
 
-  // Corregir doble llamado a fetchProducts
-  const fetchProducts = async (params) => {
-    try {
-      const response = await getProducts(1, 10, params.ordering, params.selectedCategories, params.selectedSubCategories)
-      setTotalProducts(response.totalProducts)
-    } catch (error) {
-      console.error(error)
-    }
-  }
-
-  useEffect(() => {
-    const queryParams = getQueryParams()
-    setFilterParams((prevParams) => ({ ...prevParams, ...queryParams }))
-    fetchProducts(queryParams)
-  }, [location.search])
-
-  useEffect(() => {
-    const filterTitle = getFilterTitle(location.pathname)
-    setFilterParams((prevParams) => ({ ...prevParams, filterTitle }))
-  }, [location.pathname])
-
   const handleParamChange = (newParams) => {
     setFilterParams((prevParams) => ({ ...prevParams, ...newParams }))
-    updateUrlParams({ ...filterParams, ...newParams })
   }
 
   return (
--- frontend/src/features/Home/pages/DefaultEcommercePage.jsx
+++ frontend/src/features/Home/pages/DefaultEcommercePage.jsx
@@ -1,6 +1,5 @@
 import { useEffect, useState } from 'react'
 import { useProductsAPI } from '../hooks/useProductsAPI'
-import { useNavigate, useLocation } from 'react-router-dom'
 import { Products } from '../components/Products/Products'
 import { Pagination } from '@nextui-org/react'
 
@@ -17,45 +16,6 @@ export function DefaultEcommercePage () {
     selectedSubCategories: []
   })
 
-  const navigate = useNavigate()
-  const location = useLocation()
-
-  const updateUrlParams = (params) => {
-    const searchParams = new URLSearchParams(location.search)
-    searchParams.set('page', params.currentPage)
-    searchParams.set('pageSize', params.pageSize)
-    if (params.ordering) {
-      searchParams.set('ordering', params.ordering)
-    } else {
-      searchParams.delete('ordering')
-    }
-    if (params.selectedCategories.length > 0) {
-      searchParams.set('categories', params.selectedCategories.join(','))
-    } else {
-      searchParams.delete('categories')
-    }
-    if (params.selectedSubCategories.length > 0) {
-      searchParams.set('sub_categories', params.selectedSubCategories.join(','))
-    } else {
-      searchParams.delete('sub_categories')
-    }
-    navigate({
-      pathname: location.pathname,
-      search: searchParams.toString()
-    })
-  }
-
-  const getQueryParams = () => {
-    const searchParams = new URLSearchParams(location.search)
-    const currentPage = parseInt(searchParams.get('page')) || 1
-    const pageSize = parseInt(searchParams.get('pageSize')) || 12
-    const ordering = searchParams.get('ordering') || ''
-    const selectedCategories = searchParams.get('categories') ? searchParams.get('categories').split(',').map(Number) : []
-    const selectedSubCategories = searchParams.get('sub_categories') ? searchParams.get('sub_categories').split(',').map(Number) : []
-    return { currentPage, pageSize, ordering, selectedCategories, selectedSubCategories }
-  }
-
-  // Corregir doble llamado a fetchProducts
   const fetchProducts = async (params) => {
     try {
       setState((prevState) => ({ ...prevState, isLoading: true }))
@@ -73,15 +33,11 @@ export function DefaultEcommercePage () {
   }
 
   useEffect(() => {
-    const queryParams = getQueryParams()
-    setState((prevState) => ({ ...prevState, ...queryParams }))
-    fetchProducts(queryParams)
-  }, [location.search])
+    fetchProducts(state)
+  }, [state.currentPage, state.pageSize, state.ordering, state.selectedCategories, state.selectedSubCategories])
 
   const handleParamChange = (newParams) => {
-    const updatedParams = { ...state, ...newParams }
-    setState(updatedParams)
-    updateUrlParams(updatedParams)
+    setState((prevState) => ({ ...prevState, ...newParams }))
   }
 
   return (
