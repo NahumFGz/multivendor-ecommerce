ff53e272a45ce49bdf2631a01f4c673187ab7897
NahumFG <ngutierrez2507@gmail.com>
2024-07-16 11:57:24 -0500

    frontend: new account hooks

---
+++ frontend/src/features/Account/hooks/useAccountProfileAPI.jsx
@@ -0,0 +1,27 @@
+import { useAuthStore } from '../../../store/AuthStore'
+import { getAccountApi, patchAccountApi } from '../../../services/api/account/profileAPI'
+
+export function useAccountProfileAPI () {
+  const token = useAuthStore((state) => state.token)
+  const profile = useAuthStore((state) => state.profile)
+
+  const getAccountApiCall = async () => {
+    try {
+      const res = await getAccountApi(token, profile.id)
+      return res
+    } catch (error) {
+      throw new Error('Get account failed')
+    }
+  }
+
+  const patchAccountApiCall = async (accountData) => {
+    try {
+      const res = await patchAccountApi(token, profile.id, accountData)
+      return res
+    } catch (error) {
+      throw new Error('Patch account failed')
+    }
+  }
+
+  return { getAccountApiCall, patchAccountApiCall }
+}
