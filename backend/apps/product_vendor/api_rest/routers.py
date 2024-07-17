e924968091e928b56c2e3b2e97050664b1be0f30
NahumFG <ngutierrez2507@gmail.com>
2024-07-17 14:44:29 -0500

    backend: modificación de permisos de clases

--- backend/apps/product_vendor/api_rest/routers.py
+++ backend/apps/product_vendor/api_rest/routers.py
@@ -1,18 +1,11 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
-from .views import ProductVendorListView
+from .views import ProductVendorViewSet
 
 router = DefaultRouter()
-router.register(r"product-vendors", ProductVendorListView)
+router.register(r"product-vendors", ProductVendorViewSet)
 
 urlpatterns = [
     path("", include(router.urls)),
-    # path("product-vendors/", ProductVendorListView.as_view(), name="product-vendor-list"),
-    # path("product-vendors/<int:pk>/", ProductVendorCRUDView.as_view(), name="product-vendor-crud"),
-    # path(
-    #     "product-vendors/<slug:slug>/",
-    #     ProductVendorDetailView.as_view(),
-    #     name="product-vendor-detail",
-    # ),
 ]
--- backend/apps/product_vendor/api_rest/views.py
+++ backend/apps/product_vendor/api_rest/views.py
@@ -1,17 +1,16 @@
 from django_filters.rest_framework import DjangoFilterBackend
-from rest_framework import generics, mixins, viewsets
-from rest_framework.exceptions import NotFound
+from rest_framework import viewsets
 from rest_framework.filters import OrderingFilter, SearchFilter
-from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from rest_framework.permissions import IsAuthenticated
 
 from ..models import ProductVendor
 from .serializers import ProductVendorSerializer
 
 
-class ProductVendorListView(viewsets.ModelViewSet):
+class ProductVendorViewSet(viewsets.ModelViewSet):
     queryset = ProductVendor.objects.all()
     serializer_class = ProductVendorSerializer
-    permission_classes = [IsAuthenticatedOrReadOnly]
+    permission_classes = [IsAuthenticated]
     filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
     http_method_names = ["get", "post", "patch", "delete"]
     pagination_class = None
@@ -25,3 +24,12 @@ class ProductVendorListView(viewsets.ModelViewSet):
     # Configurar los campos de ordenamiento y ordenamiento predeterminado
     ordering_fields = ["price", "rating", "created_at"]
     ordering = ["created_at"]
+
+    def get_queryset(self):
+        user = self.request.user
+        if user.is_superuser:
+            return ProductVendor.objects.all()
+        elif user.is_authenticated:
+            return ProductVendor.objects.filter(vendor=user)
+        else:
+            return ProductVendor.objects.none()
